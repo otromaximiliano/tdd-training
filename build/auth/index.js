5619d625c1a37f5c39cf73f8c0e487248af7e3f5
Maximiliano Ayala <lindolfomaximiliano@gmail.com>
2021-04-10 19:56:08 -0300

    Delete build directory

--- build/auth/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-var index_routes_1 = require("./index.routes");
-Object.defineProperty(exports, "auth", { enumerable: true, get: function () { return index_routes_1.auth; } });
-//# sourceMappingURL=index.js.map
