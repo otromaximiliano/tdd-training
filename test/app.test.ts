768e896e1177990ece65aaf6e5f1676ceb72ba1c
Ayala Aguirre Lindolfo Maximiliano <lindolfomaximiliano@gmail.com>
2021-04-10 19:21:28 -0300

    init test

--- test/app.test.ts
+++ test/app.test.ts
@@ -2,10 +2,20 @@ import request from 'supertest';
 import app from '../src/app';
 
 describe('GET /auth', () => {
-  it('Request /auth should return ', async () => {
-    const data = {dataAgregada: "Dta dhgdhdhd"};
+  it('Request /auth should return msg should username and password ', async () => {
+    const data = {username: "yourUsername", password:"yourPassword"};
     const result = await request(app).get('/auth').send();
     expect(result.status).toBe(200);
     expect(result.body).toEqual(data);
   });
+});
+
+describe('POST /auth', () => {
+  it('Request /auth should return msg should username and password ', async () => {
+    const data = {username: "otromaximiliano", password:"10783729"};
+    const result = await request(app).post('/auth').send(data);
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual(data);
+  });
+  
 });
