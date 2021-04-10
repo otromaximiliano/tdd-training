2efe12c37cc424d345248080ac2b1f19d1b58647
Ayala Aguirre Lindolfo Maximiliano <lindolfomaximiliano@gmail.com>
2021-04-10 14:10:46 -0300

    change app index

--- dist/auth/index.controllers.js
+++ dist/auth/index.controllers.js
@@ -1,2 +1,18 @@
 "use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguY29udHJvbGxlcnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9hdXRoL2luZGV4LmNvbnRyb2xsZXJzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.controller = void 0;
+const controller = (data) => __awaiter(void 0, void 0, void 0, function* () {
+    let dataProcess = Object.assign(Object.assign({}, data), { dataAgregada: "Dta dhgdhdhd" });
+    return dataProcess;
+});
+exports.controller = controller;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguY29udHJvbGxlcnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9hdXRoL2luZGV4LmNvbnRyb2xsZXJzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OztBQUFPLE1BQU0sVUFBVSxHQUFHLENBQU8sSUFBUyxFQUFFLEVBQUU7SUFDMUMsSUFBSSxXQUFXLG1DQUFRLElBQUksS0FBRSxZQUFZLEVBQUUsY0FBYyxHQUFFLENBQUM7SUFDNUQsT0FBTyxXQUFXLENBQUM7QUFDdkIsQ0FBQyxDQUFBLENBQUM7QUFIVyxRQUFBLFVBQVUsY0FHckIifQ==
--- dist/auth/index.routes.js
+++ dist/auth/index.routes.js
@@ -1,16 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = void 0;
 const express_1 = __importDefault(require("express"));
+const index_controllers_1 = require("./index.controllers");
 exports.auth = express_1.default.Router();
-exports.auth.get("/", (req, res) => {
+exports.auth.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        res.send("Hi in Auth route");
+        const response = yield index_controllers_1.controller(Object.assign({}, req.query));
+        res.send(response);
     }
     catch (error) {
+        console.log(error);
     }
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXgucm91dGVzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vYXV0aC9pbmRleC5yb3V0ZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsc0RBQThCO0FBRWpCLFFBQUEsSUFBSSxHQUFvQixpQkFBTyxDQUFDLE1BQU0sRUFBRSxDQUFDO0FBRXRELFlBQUksQ0FBQyxHQUFHLENBQUMsR0FBRyxFQUFFLENBQUMsR0FBRyxFQUFFLEdBQUcsRUFBRSxFQUFFO0lBQ3pCLElBQUk7UUFDRixHQUFHLENBQUMsSUFBSSxDQUFDLGtCQUFrQixDQUFDLENBQUE7S0FDN0I7SUFBQyxPQUFPLEtBQUssRUFBRTtLQUVmO0FBQ0gsQ0FBQyxDQUFDLENBQUEifQ==
+}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXgucm91dGVzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vYXV0aC9pbmRleC5yb3V0ZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7O0FBQUEsc0RBQThCO0FBQzlCLDJEQUFpRDtBQUNwQyxRQUFBLElBQUksR0FBb0IsaUJBQU8sQ0FBQyxNQUFNLEVBQUUsQ0FBQztBQUV0RCxZQUFJLENBQUMsR0FBRyxDQUFDLEdBQUcsRUFBRSxDQUFPLEdBQUcsRUFBRSxHQUFHLEVBQUUsRUFBRTtJQUM3QixJQUFJO1FBQ0EsTUFBTSxRQUFRLEdBQUcsTUFBTSw4QkFBVSxtQkFBTSxHQUFHLENBQUMsS0FBSyxFQUFHLENBQUM7UUFDcEQsR0FBRyxDQUFDLElBQUksQ0FBQyxRQUFRLENBQUMsQ0FBQztLQUN0QjtJQUFDLE9BQU8sS0FBSyxFQUFFO1FBQ1osT0FBTyxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsQ0FBQztLQUN0QjtBQUNMLENBQUMsQ0FBQSxDQUFDLENBQUMifQ==
